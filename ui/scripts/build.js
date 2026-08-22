2b3ee814fb25c7445ff25b3266292e9452a15a06
levisantosp <levi.pires2005@gmail.com>
2026-08-22 19:42:43 -0300

    chore: simplify index generation script

--- ui/scripts/build.js
+++ ui/scripts/build.js
@@ -1,13 +1,9 @@
-import { existsSync, readdirSync, unlinkSync, writeFileSync } from 'node:fs'
+import { readdirSync, writeFileSync } from 'node:fs'
 
 let content = ''
 
 for (const file of readdirSync('./src/components')) {
   content += `export * from './components/${file.replace('.tsx', '')}'\n`
-}
-
-if (existsSync('./src/index.ts')) {
-  unlinkSync('./src/index.ts')
 }
 
 writeFileSync('./src/index.ts', content, 'utf-8')
