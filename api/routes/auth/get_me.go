bb9c1a3732386d238b9736859f782bce47a7da57
levisantosp <levi.pires2005@gmail.com>
2026-08-23 18:05:55 -0300

    feat: add get me route

---
+++ api/routes/auth/get_me.go
@@ -0,0 +1,43 @@
+package auth
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/levisantosp/altamira-participa/api/plugins"
+	"github.com/levisantosp/altamira-participa/api/redis"
+	"github.com/levisantosp/altamira-participa/api/utils"
+)
+
+type GetMeOutput struct {
+	Body plugins.Session
+}
+
+func GetMe(ctx context.Context, input *struct {
+	Session http.Cookie `cookie:"session"`
+},
+) (*GetMeOutput, error) {
+	if input.Session.Name == "" {
+		return nil, huma.Error404NotFound("Not Found")
+	}
+
+	raw, err := redis.Client.Get(ctx, "session:"+input.Session.Value).Result()
+	if err != nil {
+		return nil, utils.LogErr(
+			huma.Error500InternalServerError("Internal Server Error"),
+		)
+	}
+
+	var session plugins.Session
+	if err := json.Unmarshal([]byte(raw), &session); err != nil {
+		return nil, utils.LogErr(
+			huma.Error500InternalServerError("Internal Server Error"),
+		)
+	}
+
+	return &GetMeOutput{
+		Body: session,
+	}, nil
+}
--- api/routes/auth/routes.go
+++ api/routes/auth/routes.go
@@ -6,4 +6,5 @@ func Routes(api huma.API) {
 	group := huma.NewGroup(api, "/auth")
 	huma.Post(group, "/sign-in/email", SignInWithEmail)
 	huma.Post(group, "/sign-up/email", SignUpWithEmail)
+	huma.Get(group, "/me", GetMe)
 }
