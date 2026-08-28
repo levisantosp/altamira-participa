f6402a5a69c1d39dbb9098cf988276f05e64f735
levisantosp <levi.pires2005@gmail.com>
2026-08-28 01:06:02 -0300

    fix: add authentication middleware to issues routes

--- api/routes/issues/routes.go
+++ api/routes/issues/routes.go
@@ -1,8 +1,13 @@
 package issues
 
-import "github.com/danielgtaylor/huma/v2"
+import (
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/levisantosp/altamira-participa/api/middlewares"
+)
 
 func Routes(api huma.API) {
 	group := huma.NewGroup(api, "/issues")
+	group.UseMiddleware(middlewares.Auth(api, false))
+
 	huma.Get(group, "", GetIssues)
 }
