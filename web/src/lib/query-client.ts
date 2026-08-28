54af81a2c6b31b025411902311e38bb8effa0b0f
levisantosp <levi.pires2005@gmail.com>
2026-08-27 23:04:59 -0300

    feat: enhance queryClient with error handling for query and mutation caches

--- web/src/lib/query-client.ts
+++ web/src/lib/query-client.ts
@@ -1,3 +1,28 @@
-import { QueryClient } from '@tanstack/react-query'
+import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query'
+import { getAuthMeQueryKey, ResponseError } from 'api-client'
+import { toast } from 'sonner'
 
-export const queryClient = new QueryClient()
+const ignoredKeys = new Set([JSON.stringify(getAuthMeQueryKey())])
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError(err, query) {
+      if (ignoredKeys.has(JSON.stringify(query.queryKey))) return
+
+      if (err instanceof ResponseError) {
+        toast.error(err.response.data.detail)
+      } else if (err instanceof Error) {
+        toast.error(err.message)
+      }
+    }
+  }),
+  mutationCache: new MutationCache({
+    onError(err) {
+      if (err instanceof ResponseError) {
+        toast.error(err.response.data.detail)
+      } else if (err instanceof Error) {
+        toast.error(err.message)
+      }
+    }
+  })
+})
