5264e946a6c104b204e91f2a7404fb58c32f22f8
levisantosp <levi.pires2005@gmail.com>
2026-09-11 06:10:56 -0300

    fix: remove unused status field from CreateIssue input struct

--- api/routes/issues/create_issue.go
+++ api/routes/issues/create_issue.go
@@ -6,7 +6,6 @@ import (
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/levisantosp/altamira-participa/api/db"
 	"github.com/levisantosp/altamira-participa/api/dtos"
-	"github.com/levisantosp/altamira-participa/api/ent/generated/issue"
 	"github.com/levisantosp/altamira-participa/api/middlewares"
 	"github.com/levisantosp/altamira-participa/api/utils"
 )
@@ -21,7 +20,6 @@ func CreateIssue(
 		Body struct {
 			Title       string       `json:"title" maxLength:"72" minLength:"3" required:"true"`
 			Description string       `json:"description" maxLength:"65000" minLength:"10" required:"true"`
-			Status      issue.Status `json:"status,omitempty" enum:"open,closed,in_review" default:"open"`
 		}
 	},
 ) (*CreateIssueOutput, error) {
@@ -30,7 +28,6 @@ func CreateIssue(
 	issue, err := db.Client.Issue.Create().
 		SetTitle(input.Body.Title).
 		SetDescription(input.Body.Description).
-		SetStatus(input.Body.Status).
 		SetUserID(userCtx.ID).
 		Save(ctx)
 	if err != nil {
